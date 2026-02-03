e21f84e8f9af07bedce0a90c26e5bb8305e4e0b4
tonymushah <tonyramanantsalama@gmail.com>
2026-02-03 15:46:15 +0300

    Update estate_property.py

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,26 +1,45 @@
-from odoo import models, fields
 from datetime import timedelta
 
+from odoo import fields, models
+
+
 def date_availability_default():
-    current_date = fields.Date().today();
-    return current_date + timedelta(weeks = 13)
+    current_date = fields.Date().today()
+    return current_date + timedelta(weeks=13)
+
 
 class Property(models.Model):
     _name = "estate.property"
     _description = "The Real Estate Property"
 
-    name = fields.Char("Property name", required = True)
+    name = fields.Char("Property name", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy = False, default = date_availability_default() )
-    expected_price = fields.Float(required = True)
-    selling_price = fields.Float(readonly = True, copy = False)
-    bedrooms = fields.Integer(default = 2)
+    date_availability = fields.Date(copy=False, default=date_availability_default())
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(selection = [("north","North"), ("south","South"), ("east","East"), ("west","West")])
-    active = fields.Boolean(default = True)
-    state = fields.Selection(selection = [("new", "New"), ("offer_received", "Offer Received"), ("offer_accepted","Offer Accepted"), ("sold", "Sold"), ("cancelled", "Cancelled")], default = "new")
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
+    )
