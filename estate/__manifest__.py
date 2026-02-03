c4aa919867d2aa1c618d6c4f288b1c4b85eb17ce
tonymushah <tonyramanantsalama@gmail.com>
2026-02-03 15:51:02 +0300

    Update __manifest__.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,10 +3,10 @@
     "version": "1.0",
     "depends": ["base"],
     "category": "Tutorials",
-    'application': True,
+    "application": True,
     "data": [
         "views/estate_property_views.xml",
         "views/estate_menus.xml",
-        "security/ir.model.access.csv"
-    ]
+        "security/ir.model.access.csv",
+    ],
 }
