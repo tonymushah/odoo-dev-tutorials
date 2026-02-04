66e86027d04f182566b3862e831efdd9670032a4
tonymushah <tonyramanantsalama@gmail.com>
2026-02-04 07:09:42 +0300

    Update estate_property.py

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -50,5 +50,5 @@ class Property(models.Model):
     buyer_partner_id = fields.Many2one("res.partner", "Buyer", copy=False)
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many(
-        "estate.property.offer", "buyer_partener_id", string="Offers"
+        "estate.property.offer", "buyer_partner_id", string="Offers"
     )
