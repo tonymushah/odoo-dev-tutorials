2435fac3f056a7a41f3442fd9fe5b545de3af6a1
tonymushah <tonyramanantsalama@gmail.com>
2026-02-04 10:59:50 +0300

    added `validity` and `date_dateline` field

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from datetime import timedelta
+
+from odoo import api, fields, models
 
 
 class PropertyOffer(models.Model):
@@ -11,3 +13,10 @@ class PropertyOffer(models.Model):
     )
     buyer_partner_id = fields.Many2one("res.partner", required=True, string="Buyer")
     property_id = fields.Many2one("estate.property", required=True, string="Property")
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline")
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for offer in self:
+            offer.date_deadline = offer.create_date + timedelta(days=offer.validity)
