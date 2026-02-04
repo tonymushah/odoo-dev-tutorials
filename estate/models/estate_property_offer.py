e96b482dbe72b5eb398c15933451ec673c06c409
tonymushah <tonyramanantsalama@gmail.com>
2026-02-04 11:10:32 +0300

    Update estate_property_offer.py

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -14,9 +14,16 @@ class PropertyOffer(models.Model):
     buyer_partner_id = fields.Many2one("res.partner", required=True, string="Buyer")
     property_id = fields.Many2one("estate.property", required=True, string="Property")
     validity = fields.Integer(default=7)
-    date_deadline = fields.Date(compute="_compute_date_deadline")
+    date_deadline = fields.Date(
+        compute="_compute_date_deadline", inverse="_inverse_date_deadline"
+    )
 
     @api.depends("validity")
     def _compute_date_deadline(self):
         for offer in self:
             offer.date_deadline = offer.create_date + timedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            t_delta = offer.date_deadline - offer.create_date
+            offer.validity = t_delta.days
