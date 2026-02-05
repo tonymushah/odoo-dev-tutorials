d35d8f0c38763956f0e291a0e752930f8efeaedd
tonymushah <tonyramanantsalama@gmail.com>
2026-02-05 16:17:32 +0300

    Update estate_property_tag.py

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 class PropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Real Estate Property Tag"
+    _order = "name"
 
     name = fields.Char("Property Tag", required=True)
     _unique_name = models.Constraint("UNIQUE(name)", "The name must be unique")
