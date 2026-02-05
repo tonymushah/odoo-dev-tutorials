3d85e6b78b2dff9137cabd97deaacb0be3c4a958
tonymushah <tonyramanantsalama@gmail.com>
2026-02-05 16:28:10 +0300

    Update estate_property_type.py

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class PropertyType(models.Model):
     _name = "estate.property.type"
     _description = "The Real Estate Property Types"
-    _name = "name"
+    _order = "name"
 
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
